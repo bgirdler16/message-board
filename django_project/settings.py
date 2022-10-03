7806e7e699478e1993264e5c09602c204497beac
Blake Girdler <blake1.girdler@umconnect.umt.edu>
2022-10-03 15:51:35 -0600

    added Heroku functionality

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cufqw1=-k%d^=_2pr9^c+6u3v4v@$!nkg_266==*)lh%c+xxb=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokupapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
